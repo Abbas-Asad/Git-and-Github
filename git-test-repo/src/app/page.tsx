9fafaf2f3d4b21c1d11778f26624df1b40bfb4ef
M-Abbas2208b <abbas2208b@aptechsite.net>
2025-01-04 00:47:53 +0500

    add h2

--- git-test-repo/src/app/page.tsx
+++ git-test-repo/src/app/page.tsx
@@ -1,7 +1,8 @@
 export default function Home() {
   return (
     <main className='container  mx-auto p-4 text-center text-white'>
-      <h1 className='text-4xl font-bold'>This is main page</h1>
+      <h1 className='text-4xl font-bold'>This is heading 1</h1>
+      <h2 className='text-4xl font-bold'>This is heading 2</h2>
     </main>
   );
 }
